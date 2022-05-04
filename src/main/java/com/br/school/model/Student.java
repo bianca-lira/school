4bf4b72e97fe29654167f49cb6a1b6dc367fd4ce
bianca-lira <bianca.de.l.pereira@accenture.com>
2022-05-04 14:01:23 -0300

    inclusão dos atributos das entidades

--- src/main/java/com/br/school/model/Student.java
+++ src/main/java/com/br/school/model/Student.java
@@ -8,7 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.time.LocalDate;
 
 
 @EqualsAndHashCode(callSuper = true)
@@ -22,4 +26,22 @@ public class Student extends BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String responsiblePhone;
+
+    @NotBlank
+    private Long registration;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private LocalDate birthDate;
 }
